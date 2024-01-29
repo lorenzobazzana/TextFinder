cf20a635d2a01bbc8e5ff8537252fb66e87350a7
lorenzobazzana <lorenzo.bazzana@gmail.com>
2024-01-29 16:30:56 +0100

    Initial Commit

---
+++ Progetto/ProgettoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgettoApp.swift
+//  Progetto
+//
+//  Created by Lorenzo on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ProgettoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
