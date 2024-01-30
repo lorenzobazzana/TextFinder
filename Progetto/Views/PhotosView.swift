400bf789c72a0f086df50de15841c7166ea64cac
lorenzobazzana <lorenzo.bazzana@gmail.com>
2024-01-30 17:06:46 +0100

    Created HomeView, plus PhotosView prototype

---
+++ Progetto/Views/PhotosView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotosView.swift
+//  Progetto
+//
+//  Created by Lorenzo on 30/01/24.
+//
+
+import SwiftUI
+
+struct PhotosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PhotosView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotosView()
+    }
+}
