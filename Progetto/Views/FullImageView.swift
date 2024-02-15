4a704d24987a03d215962669dff1e6a9cef77286
lorenzozanolin <lorenzozanolin@hotmail.com>
2024-02-15 19:42:52 +0100

    update

--- Progetto/Views/FullImageView.swift
+++ Progetto/Views/FullImageView.swift
@@ -8,16 +8,48 @@
 import SwiftUI
 
 struct FullImageView: View {
+    @Binding var show:Bool
+    @State var viewState : CGSize = .zero
+    let img: UIImage
+    //let id:String
     
-    let img: IdentifiableImage
-    let width: Int
-    let height: Int
+    
+    init(img: UIImage, show : Binding<Bool>) {
+        //self.img = UIImage(data: img.data as Data) ?? UIImage()
+        self.img=img
+        self._show = show
+        //self.id = id
+    }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            //Text("\(id)")
+            Image(uiImage: self.img)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Apply filter")
+            })
+        }.gesture(  //used to close the fullImage view when the user scrolls downward on top 
+            DragGesture()
+                .onChanged{
+                    value in
+                    if value.startLocation.y > UIScreen.main.bounds.height + 100{
+                        viewState = value.translation
+                    }
+                }
+                .onEnded({value in
+                if viewState.height < 50{
+                    show = false    //close the view
+                }
+                withAnimation{viewState = .zero}})
+        )
+            
     }
 }
 
 #Preview {
-    FullImageView(img: IdentifiableImage(rawData: NSData()), width: 0, height: 0)
+    FullImageView(img: UIImage(),show:.constant(false))
+    //IdentifiableImage(rawData: NSData())
 }
